665900628daf73fde07f9f9405a482218b1b4ae3
James Bester <jamesthomasbester@gmail.com>
2022-05-30 20:32:51 +1000

    route added

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
 
