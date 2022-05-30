712b0f0ecf4ebcfcdf17f37223632ab1cc3b4ac6
James Bester <46641259+jamesthomasbester@users.noreply.github.com>
2022-05-30 21:35:41 +1000

    updated to add helper

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const helpers = require('./util/helpers');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
