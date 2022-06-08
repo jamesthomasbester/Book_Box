7faa12ad9fbe5b25cd13f18df466b02f2b6d1754
james <jamesthomasbester@gmail.com>
2022-06-09 02:53:23 +1000

    postgres should work now

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const fs = require('fs');
+const { Http2ServerResponse } = require('http2');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
+const sslRedirect = require('heroku-ssl-redirect').default
 
+app.use(sslRedirect(['production'], 301));
 app.use(session({
   secret: 'testing',
   resave: true,
@@ -27,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
