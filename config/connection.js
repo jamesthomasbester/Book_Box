a1a2c7f1e1dd5ce1d3ce932cad50bcf645295be9
Michael Auricht <michaelauricht@live.com.au>
2022-06-09 14:50:00 +1000

    michael changes

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize = new Sequelize(
-    'de3q1085p47fp1',
-    'fdsehoaamnwltz',
-    '960887e5731f7a461c065fd23bbdd2111930b726c5a21aac2b938a1633c093c5',
-{
-    host: 'ec2-34-231-221-151.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    port: 5432
-}
-);
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
