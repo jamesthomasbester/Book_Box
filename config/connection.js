3c7f27253f2d59fac72d76aae49841a4ead6a234
James Bester <jamesthomasbester@gmail.com>
2022-06-14 21:42:30 +1000

    runs locally on postgres

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ let sequelize = new Sequelize(
     {
         host: 'ec2-34-231-221-151.compute-1.amazonaws.com',
         dialect: 'postgres',
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        username: 'fdsehoaamnwltz',
+        password: '960887e5731f7a461c065fd23bbdd2111930b726c5a21aac2b938a1633c093c5',
+        database: 'de3q1085p47fp1',
         port: 5432,
         dialectOptions:{
             ssl: {
