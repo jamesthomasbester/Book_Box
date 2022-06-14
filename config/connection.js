4c6a07fe1d5ff0b460f3b8aea89c5e76d9d361de
James Bester <jamesthomasbester@gmail.com>
2022-06-14 21:33:53 +1000

    runs locally on postgres

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,6 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
     {
         host: 'ec2-34-231-221-151.compute-1.amazonaws.com',
         dialect: 'postgres',
