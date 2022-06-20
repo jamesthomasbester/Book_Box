708acfb116f279aa235096eebd041940c67d47df
James Bester <jamesthomasbester@gmail.com>
2022-06-20 19:38:55 +1000

    test

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const User = require('./User');
-const Book = require('./Book');
-const Favourite = require('./Favourite');
-const Cart = require('./Cart');
+const User = require('./User.js');
+const Book = require('./Book.js');
+const Favourite = require('./Favourite.js');
+const Cart = require('./Cart.js');
 
 module.exports = { User, Book, Favourite, Cart };
 
